a3200f2b8914c3b7e34981907f88b65a32862f9a
mardurann <160878935+mardurann@users.noreply.github.com>
2024-02-22 10:17:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.title(" Mi Primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis apliaciones para interfaces multimodales")
-st.write("Facilmente puedo realizar backend frontend")
+st.write("Facilmente puedo realizar backend frontend.")
 image = Image.open('imagen1.jpeg')

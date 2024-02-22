896d373d30411fb09cb08a01efb01067c3d7c9e7
mardurann <160878935+mardurann@users.noreply.github.com>
2024-02-22 10:15:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.title(" Mi Primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis apliaciones para interfaces multimodales")
 st.wrtie("Facilmente puedo realizar backend frontend")
-image = Image.open("imagen1.jpeg")
+image = Image.open('imagen1.jpeg')

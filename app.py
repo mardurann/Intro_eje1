6dd63708c59800293519680a9a7ca647882897f1
mardurann <160878935+mardurann@users.noreply.github.com>
2024-02-22 10:29:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ image = Image.open('imagen1.jpeg')
 
 st.image(image, caption= 'Interfaces multimodales')
 
-
 texto = st.text_input('Escribe algo', 'Este es mi texto')
 st.write('El texto escrito es,' texto)
 

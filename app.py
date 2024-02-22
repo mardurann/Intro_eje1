2908c5f481438a1a3f22fded60e142732772703e
mardurann <160878935+mardurann@users.noreply.github.com>
2024-02-22 10:46:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,29 @@ col1, col2 = st.columns(2)
 
 with col1:
   st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia del usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('CORRECTO')
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ('Visual','Auditiva','Táctil'))
+  if modo == 'Visual'
+    st.write('La vista es fundamental para tu interfaz')
+  if modo == 'Auditiva'
+    st.write == ('La audición es fundamental para tu interfaz')
+  if modo == 'Táctil'
+    st.write == ('El tacto es fundamental para tu interfaz')
+
+st.subheader("Uso de botones")
+if st.button('Presione el botón'):
+  st.write('Gracias por presionar')
+else
+  st.write('No has presionado aún')
+
+
+
+
+
           
